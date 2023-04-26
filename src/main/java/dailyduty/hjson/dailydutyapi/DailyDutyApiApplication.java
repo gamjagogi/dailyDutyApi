590da6a8bf9f2e8a7f44380dfd34521833176a23
mara <gudwns258@gmail.com>
2023-04-27 00:48:46 +0900

    initial commit

---
+++ src/main/java/dailyduty/hjson/dailydutyapi/DailyDutyApiApplication.java
@@ -0,0 +1,13 @@
+package dailyduty.hjson.dailydutyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyDutyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyDutyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dailyduty/hjson/dailydutyapi/DailyDutyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dailyduty.hjson.dailydutyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyDutyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
